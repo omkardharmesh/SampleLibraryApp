102ffeb999e85adff6f5127f82b526f3d136be8d
dharmesh_omkar <omkar.d@eloelo.in>
2025-02-18 16:03:46 +0530

    Initial commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+b
+2app/src/main/java/com/samplemodule/MainActivity.kt,b/e/be0eacd660205eaf98cbe48e71a014e220314b16
+Z
+*samplelibrary/src/main/AndroidManifest.xml,2/5/257565b02a2e6504eef05b2a49cb861eaf84a9d0
+k
+;samplelibrary/src/main/res/layout/activity_sample_toast.xml,4/7/4724476e9a36416237c85c9e1e2a5f8a2b32a501
+t
+Dsamplelibrary/src/main/java/com/samplelibrary/SampleToastActivity.kt,6/8/68e18062edd5391dc5de422d30ed16455d3e65ac
+J
+samplelibrary/build.gradle,1/1/11e7d1fb7a263386b60fa58b2ff7b523cb139e48
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
---
+++ samplelibrary/src/main/java/com/samplelibrary/SampleToastActivity.kt
@@ -0,0 +1,22 @@
+package com.samplelibrary
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SampleToastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sample_toast)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Toast.makeText(this, "Toast in my sample module", Toast.LENGTH_LONG).show()
+    }
+}
